6258ca6e52a5f8764e7e272dac0c76e1d446e299
li-jonathan <li.jonathancj@gmail.com>
2025-04-17 21:19:46 -0500

    restart

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="col-start-2 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="font-bold text-3xl tracking-tighter mb-4">jonathanli.dev</h1>
-      <p>frontend developer</p>
-    </main>
-  );
-}
